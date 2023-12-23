a6e49c528aecda1b68cc8010cc8932cfb8ba475f
Erik Urbanski <erikurbanski@gmail.com>
2023-12-23 20:41:40 -0300

    feature: criando controllers e seus respectivos testes para o módulo de livros.

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -3,12 +3,131 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+use App\Http\Requests\StoreBookRequest;
+use App\Http\Requests\UpdateBookRequest;
+use App\Http\Resources\AuthorResource;
+use App\Http\Resources\BookResource;
+
+use Core\UseCase\Book\GetBookUseCase;
+use Core\UseCase\Book\CreateBookUseCase;
+use Core\UseCase\Book\DeleteBookUseCase;
 use Core\UseCase\Book\ListBooksUseCase;
+use Core\UseCase\Book\UpdateBookUseCase;
+use Core\UseCase\DTO\Book\Input\RequestGetBookDTO;
+use Core\UseCase\DTO\Book\Input\RequestListBooksDTO;
+use Core\UseCase\DTO\Book\Input\RequestUpdateBookDTO;
+use Core\UseCase\DTO\Book\Input\RequestCreateBookDTO;
+use Core\Domain\Exception\EntityValidationException;
+
 
 class BookController extends Controller
 {
+    /**
+     * Default index action.
+     * @param Request $request
+     * @param ListBooksUseCase $useCase
+     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
+     */
     public function index(Request $request, ListBooksUseCase $useCase)
     {
+        $response = $useCase->execute(
+            inputs: new RequestListBooksDTO(
+                filter: $request->get('filter', ''),
+                order: $request->get('order', 'DESC'),
+            ),
+        );
 
+        return AuthorResource::collection(
+            collect($response->items)
+        )->additional([
+            'meta' => [
+                'total' => $response->total,
+            ],
+        ]);
+    }
+
+    /**
+     * Store one book.
+     * @param StoreBookRequest $request
+     * @param CreateBookUseCase $useCase
+     * @return JsonResponse
+     * @throws EntityValidationException
+     */
+    public function store(StoreBookRequest $request, CreateBookUseCase $useCase): JsonResponse
+    {
+        $response = $useCase->execute(
+            inputs: new RequestCreateBookDTO(
+                title: $request->title,
+                publisher: $request->publisher,
+                edition: $request->edition,
+                year: $request->year,
+                value: $request->value,
+            ),
+        );
+
+        $resource = new BookResource(collect($response));
+        return $resource
+            ->response()
+            ->setStatusCode(code: Response::HTTP_CREATED);
+    }
+
+    /**
+     * Get one book.
+     * @param int $id
+     * @param GetBookUseCase $useCase
+     * @return JsonResponse
+     */
+    public function show(int $id, GetBookUseCase $useCase): JsonResponse
+    {
+        $response = $useCase->execute(
+            new RequestGetBookDTO($id),
+        );
+
+        $resource = new BookResource(collect($response));
+        return $resource
+            ->response()
+            ->setStatusCode(code: Response::HTTP_OK);
+    }
+
+    /**
+     * Update data book.
+     * @param int $id
+     * @param UpdateBookRequest $request
+     * @param UpdateBookUseCase $useCase
+     * @return JsonResponse
+     */
+    public function update(int $id, UpdateBookRequest $request, UpdateBookUseCase $useCase): JsonResponse
+    {
+        $response = $useCase->execute(
+            new RequestUpdateBookDTO(
+                id: $id,
+                title: $request->title,
+                publisher: $request->publisher,
+                edition: $request->edition,
+                year: $request->year,
+                value: $request->value,
+            ),
+        );
+
+        $resource = new BookResource(collect($response));
+        return $resource
+            ->response()
+            ->setStatusCode(code: Response::HTTP_OK);
+    }
+
+    /**
+     * Delete book.
+     * @param int $id
+     * @param DeleteBookUseCase $useCase
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(int $id, DeleteBookUseCase $useCase): \Illuminate\Http\Response
+    {
+        $useCase->execute(new RequestGetBookDTO($id));
+
+        return response()->noContent();
     }
 }
---
+++ tests/Feature/App/Http/Controller/BookControllerTest.php
@@ -0,0 +1,162 @@
+<?php
+
+namespace Tests\Feature\App\Http\Controller;
+
+use App\Http\Controllers\BookController;
+use App\Http\Requests\StoreBookRequest;
+use App\Http\Requests\UpdateBookRequest;
+use App\Models\Book;
+use App\Repositories\Eloquent\BookEloquentRepository;
+use Core\Domain\Repository\BookRepositoryInterface;
+use Core\UseCase\Author\DeleteAuthorUseCase;
+use Core\UseCase\Book\CreateBookUseCase;
+use Core\UseCase\Book\DeleteBookUseCase;
+use Core\UseCase\Book\GetBookUseCase;
+use Core\UseCase\Book\ListBooksUseCase;
+use Core\UseCase\Book\UpdateBookUseCase;
+use Tests\TestCase;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ParameterBag;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+
+use Core\UseCase\Author\GetAuthorUseCase;
+use Core\UseCase\Author\ListAuthorsUseCase;
+use Core\UseCase\Author\CreateAuthorUseCase;
+use Core\UseCase\Author\UpdateAuthorUseCase;
+use Core\Domain\Exception\EntityValidationException;
+
+use App\Models\Author;
+use App\Http\Requests\StoreAuthorRequest;
+use App\Http\Requests\UpdateAuthorRequest;
+use App\Http\Controllers\AuthorController;
+use App\Repositories\Eloquent\AuthorEloquentRepository;
+
+class BookControllerTest extends TestCase
+{
+    protected BookController $controller;
+    protected BookEloquentRepository $repository;
+
+    /**
+     * Initialize config tests.
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->controller = new BookController();
+        $this->repository = new BookEloquentRepository(new Book());
+    }
+
+    /**
+     * Test index controller.
+     * @return void
+     */
+    public function testIndex()
+    {
+        $useCase = new ListBooksUseCase($this->repository);
+        $response = $this->controller->index(
+            request: new Request(),
+            useCase: $useCase,
+        );
+
+        $this->assertInstanceOf(AnonymousResourceCollection::class, $response);
+        $this->assertArrayHasKey('meta', $response->additional);
+    }
+
+    /**
+     * Test store controller.
+     * @return void
+     * @throws EntityValidationException
+     */
+    public function testStore()
+    {
+        $useCase = new CreateBookUseCase($this->repository);
+        $request = new StoreBookRequest();
+
+        $request->headers->set('content-type', 'application/json');
+        $request->setJson(
+            new ParameterBag([
+                'title' => 'SOLID Principle',
+                'publisher' => 'Pandas',
+                'edition' => 1,
+                'year' => '2023',
+                'value' => 100
+            ]),
+        );
+
+        $response = $this->controller->store($request, $useCase);
+
+        $this->assertInstanceOf(JsonResponse::class, $response);
+        $this->assertEquals(Response::HTTP_CREATED, $response->status());
+    }
+
+    /**
+     * Test show book controller.
+     * @return void
+     */
+    public function testShow()
+    {
+        $book = Book::factory()->create();
+
+        $response = $this->controller->show(
+            id: $book->id,
+            useCase: new GetBookUseCase($this->repository),
+        );
+
+        $this->assertInstanceOf(JsonResponse::class, $response);
+        $this->assertEquals(Response::HTTP_OK, $response->status());
+    }
+
+    /**
+     * Test update book controller.
+     * @return void
+     */
+    public function testUpdate()
+    {
+        $book = Book::factory()->create();
+
+        $useCase = new UpdateBookUseCase($this->repository);
+        $request = new UpdateBookRequest();
+
+        $request->headers->set('content-type', 'application/json');
+        $request->setJson(
+            new ParameterBag([
+                'name' => 'Update Author Name',
+                'title' => 'SOLID e TDD',
+                'publisher' => 'Atlas',
+                'edition' => 2,
+                'year' => '2022',
+                'value' => 129.8
+            ]),
+        );
+
+        $response = $this->controller->update(
+            id: $book->id,
+            request: $request,
+            useCase: $useCase,
+        );
+
+        $this->assertInstanceOf(JsonResponse::class, $response);
+        $this->assertEquals(Response::HTTP_OK, $response->status());
+    }
+
+    /**
+     * Test delete book controller.
+     * @return void
+     */
+    public function testDelete()
+    {
+        $book = Book::factory()->create();
+
+        $response = $this->controller->destroy(
+            id: $book->id,
+            useCase: new DeleteBookUseCase($this->repository),
+        );
+
+        $this->assertEquals(Response::HTTP_NO_CONTENT, $response->status());
+    }
+}
