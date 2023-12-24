13bb5349a72d1c5b7777401a8d8b023757783eb5
Erik Urbanski <erikurbanski@gmail.com>
2023-12-24 02:35:01 -0300

    feature: classe para transactions junto ao banco de dados

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,14 +4,30 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use App\Repositories\Transaction\DatabaseTransaction;
+use App\Repositories\Eloquent\AuthorEloquentRepository;
+
+use Core\UseCase\Interfaces\TransactionInterface;
+use Core\Domain\Repository\AuthorRepositoryInterface;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
      * @return void
      */
-    public function register()
+    public function register(): void
     {
+        $this->app->singleton(
+            AuthorRepositoryInterface::class,
+            AuthorEloquentRepository::class,
+        );
+
+        # DB Transaction:
+        $this->app->bind(
+            TransactionInterface::class,
+            DatabaseTransaction::class,
+        );
     }
 
     /**
