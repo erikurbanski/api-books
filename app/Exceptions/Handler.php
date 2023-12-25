d13f14ba95684d6b800e2c10a5bfbb98241bbb68
Erik Urbanski <erikurbanski@gmail.com>
2023-12-25 16:32:17 -0300

    feature: adicionando error handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,23 +2,27 @@
 
 namespace App\Exceptions;
 
-use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Symfony\Component\HttpFoundation\Response;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+
+use Core\Domain\Exception\EntityValidationException;
+use Core\Domain\Exception\NotFoundRegisterException;
+
+
 
 class Handler extends ExceptionHandler
 {
     /**
      * A list of the exception types that are not reported.
-     *
      * @var array<int, class-string<Throwable>>
      */
-    protected $dontReport = [
-        //
-    ];
+    protected $dontReport = [];
 
     /**
      * A list of the inputs that are never flashed for validation exceptions.
-     *
      * @var array<int, string>
      */
     protected $dontFlash = [
@@ -29,13 +33,53 @@ class Handler extends ExceptionHandler
 
     /**
      * Register the exception handling callbacks for the application.
-     *
      * @return void
      */
-    public function register()
+    public function register(): void
     {
         $this->reportable(function (Throwable $e) {
-            //
         });
+    }
+
+    /**
+     * Render errors.
+     * @param $request
+     * @param Throwable $e
+     * @return JsonResponse|null
+     */
+    public function render($request, Throwable $e): ?JsonResponse
+    {
+        $render = null;
+
+        if ($e instanceof NotFoundRegisterException) {
+            $render = $this->showError(
+                message: $e->getMessage(),
+                statusCode: Response::HTTP_NOT_FOUND,
+            );
+        }
+
+        if ($e instanceof EntityValidationException) {
+            $render = $this->showError(
+                message: $e->getMessage(),
+                statusCode: Response::HTTP_INTERNAL_SERVER_ERROR,
+            );
+        }
+
+        return $render;
+    }
+
+    /**
+     * Show message of error.
+     * @param string $message
+     * @param int $statusCode
+     * @return JsonResponse
+     */
+    private function showError(string $message, int $statusCode): JsonResponse
+    {
+        $data = [
+            'message' => $message,
+        ];
+
+        return response()->json($data, $statusCode);
     }
 }
